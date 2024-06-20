141e30e3a415dd8382143b3f573f257310903bba
simao-novais-goncalves <simao.novais.goncalves@luzsaude.pt>
2024-06-20 20:02:17 +0000

    update

--- utils.py
+++ utils.py
@@ -1,33 +1,19 @@
 import numpy as np
 import pandas as pd
+from sklearn.metrics import precision_score, recall_score, f1_score
 
-def preprocess_data(df):
-    # for an explanation on why we cleaned/preprocessed the data in this way
-    # please refer to the notebook data cleaning explained.ipynb
+def measure_metrics(model, X, y):
+    y_pred = model.predict(X)
+    f1 = f1_score(y, y_pred)
+    recall = recall_score(y, y_pred)
+    precision = precision_score(y, y_pred)
+    return {'f1_score':f1, 'recall':recall, 'precision':precision}
 
-    column_mapping = {0:'Id',
-                  1: "Clump Thickness", 
-                  2: "Uniformity of Cell Size", 
-                  3: "Uniformity of Cell Shape",
-                  4: "Clump Marginal Adhesion",
-                  5: "Single Epithelial Cell Size",
-                  6: "Bare Nuclei",
-                  7: "Bland Chromatin",
-                  8: "Normal Nucleoli",
-                  9: "Mitoses",
-                  10: "Class"}
-    
-    df = df.rename(columns=column_mapping)
-    df = df.set_index('Id')
+def display_metrics(clf, X_train, y_train, X_test, y_test):
 
-    positions_with_interrogation_mark = df['Bare Nuclei'] == '?'
-    df.loc[positions_with_interrogation_mark,'Bare Nuclei'] = np.nan
-    df['Bare Nuclei'] = df['Bare Nuclei'].astype(float) # floats are numbers in python
+    train_metrics = measure_metrics(clf,X_train,y_train)
+    test_metrics = measure_metrics(clf,X_test,y_test)
 
-    average_value = df['Bare Nuclei'].mean()
-    df['Bare Nuclei'] = df['Bare Nuclei'].fillna(average_value) # fill nan values with the average value of Bare Nuclei from the whole dataset
-    
+    metrics_data = pd.DataFrame([train_metrics,test_metrics],index=['Train metrics', 'Test metrics'])
 
-    df["Class"] = df["Class"].replace({2:0, 4:1})
-
-    return df
+    return metrics_data
